06697e55c46420a6dfe8d8bce01dadddd431ef83
JPack <168360639+jprackley@users.noreply.github.com>
2026-05-09 21:29:02 -0600

    Linking vercel to react front end

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+
 const clientDistPath = path.join(__dirname, '..', 'dist');
 
 app.use(express.json());
