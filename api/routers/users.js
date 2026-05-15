d61a3730c455ffcfd8379f80956f95c7fc2d5fe8
JPack <168360639+jprackley@users.noreply.github.com>
2026-05-15 15:59:12 -0600

    Building User APIs and Tests

--- api/routers/users.js
+++ api/routers/users.js
@@ -3,10 +3,21 @@ const CONST = require('../utils/constants');
 const C_HTTP = require('../utils/httpStatus');
 const asyncHandler = require('../utils/asyncHandler');
 const { query } = require('../db');
-const { body } = require("express-validator");
+const { body, param} = require("express-validator");
 const {handleValidation} = require("../utils/validation");
 const crypto = require("node:crypto");
 const router = express.Router();
+
+const safeUserColumns = `
+    user_id,
+    first_name,
+    last_name,
+    email,
+    last_login,
+    created_at,
+    updated_at,
+    account_role`;
+
 
 //------------------------------//
 //        CREATE User           //
@@ -29,16 +40,39 @@ router.post(
             .createHash("sha256")
             .update(req.body.password_hash)
             .digest("hex");
-        const { first_name, last_name, email, account_role, password_hash } = req.body;
+        const { first_name, last_name, email, account_role } = req.body;
 
         const sql = `
             INSERT INTO users (first_name, last_name, email, password_hash, account_role)
             VALUES ($1, $2, $3, $4, $5)
-            RETURNING *
+            RETURNING 
+                user_id,
+                first_name,
+                last_name,
+                email,
+                last_login,
+                created_at,
+                updated_at,
+                account_role
         `;
         const { rows } = await query(sql, [first_name, last_name, email, hashedPassword, account_role ]);
         res.status(C_HTTP.STATUS.CREATED).json(rows[0]);
     })
 );
+
+//------------------------------//
+//        READ User           //
+//------------------------------//
+router.get(
+    `/id`,
+    [param('id').isUUID()],
+    asyncHandler(async (req, res) => {
+        handleValidation(req, 'READ User:id - ');
+        const { id } = req.params;
+        const { rows } = await query(`SELECT ${safeUserColumns} FROM users WHERE user_id = $1`, [id]);
+        if (rows.length === 0) return res.status(C_HTTP.STATUS.NOT_FOUND).json({ error: { code: C_HTTP.REASON.NOT_FOUND, message: 'User ID not found' } });
+        res.json(rows[0]);
+    })
+)
 
 module.exports = router;
--- test/api/user.test.js
+++ test/api/user.test.js
@@ -2,17 +2,27 @@ const app = require('../../api/index')
 const {describe, test, before, after} = require("node:test");
 const C_HTTP = require('../../api/utils/httpStatus');
 const assert = require("node:assert");
-const C = require('../../api/utils/constants');
 const request = require('supertest');
 
-describe('POST /api/users', () => {
+describe('Testing /api/users', () => {
+    const users = [];
+    before(async () => {
 
-    before(async () => {});
+        const response = await request(app).post('/api/users').send({
+            first_name: "Testin",
+            last_name: "Using",
+            email: `t.user${Date.now()}@testees.com`,
+            password_hash: "password",
+            account_role: "Manager",
+        });
+        assert.equal(response.statusCode, C_HTTP.STATUS.CREATED,
+            `Expected status code ${C_HTTP.STATUS.CREATED}, got ${response.statusCode} \n ${JSON.stringify(response.body, null, 2)}`);
+    });
     after(async () => {});
 
-    describe('[test]: CREATE User', () => {
+    describe('[api]: CREATE User', () => {
 
-            test(`[test]: valid entry [expected]: status code ${C_HTTP.STATUS.CREATED}`, async () => {
+            test(`[test]: CREATE valid entry [expected]: status code ${C_HTTP.STATUS.CREATED}`, async () => {
                 const response = await request(app).post('/api/users').send({
                     first_name: "Johnny",
                     last_name: "Tester",
@@ -25,6 +35,10 @@ describe('POST /api/users', () => {
             });
 
     });
+
+    describe('[api]: READ User', () => {
+
+    })
 
 
 });
