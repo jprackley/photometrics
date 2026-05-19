01d3824fc7c91d9992ef28a508e53b2ddb091ba8
Colyma <168313954+Colyma@users.noreply.github.com>
2026-05-18 23:58:05 -0700

    Update sharedComponents.jsx

--- src/features/sharedComponents.jsx
+++ src/features/sharedComponents.jsx
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
-// Split from features/Pages.jsx to keep each page easier to maintain.
+// Shared Components.
 // -----------------------------------------------------------------------------
 
 import React, { useEffect, useMemo, useState } from "react";
