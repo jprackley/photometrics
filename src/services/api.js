10df36615917b7aa521c98d2bd74ced5ecc2a266
Colyma <168313954+Colyma@users.noreply.github.com>
2026-05-20 13:11:59 -0700

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -118,7 +118,7 @@ const API_ENDPOINTS = {
     // Implemented data source can be optimized independently of the frontend.
     //----------------------------------------------------------------------------------
     dashboard: {
-        kpis: "/dashboard/kpis",
+        kpis: "/kpi/projects/active",
         productivity: "/dashboard/productivity",
         workflow: "/dashboard/workflow",
         employeeActivity: "/dashboard/employee-activity",
