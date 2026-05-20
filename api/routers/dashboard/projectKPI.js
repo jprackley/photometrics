fce5c66723703658bf70586621706c871abaccd3
JPack <168360639+jprackley@users.noreply.github.com>
2026-05-20 16:39:23 -0600

    Testing /kpi/projects/active API route

--- api/routers/dashboard/projectKPI.js
+++ api/routers/dashboard/projectKPI.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const C_PROJECT = require('../../../utils/constants/cProjects');
-const C_KPI = require('../../../utils/constants/cKPIs');
 
 const asyncHandler = require('../../../utils/helpers/asyncHandler');
 const { verbose } = require('../../validators/queryHandler');
@@ -34,6 +33,85 @@ router.get(
         }
 
         res.json(activeProjects);
-    }))
+    })
+)
+router.get(
+    '/completed',
+    [verbose],
+    asyncHandler(async (req, res) => {
+        handleValidation(req, 'GET Completed Projects - ');
+        const { v } = req.query;
+        let completedProjects = {
+            completed: 0,
+            projects: {}
+        }
+        const params = [C_PROJECT.STATUS.COMPLETED]
+        const isCompletedSQL = `
+            SELECT *
+            FROM projects
+            WHERE completed_at IS NOT NULL
+              AND status::TEXT = $1;
+        `
+        const { rows } = await query(isCompletedSQL, params);
+
+        completedProjects.active = rows.length;
+        if ( v === 'true' ) {
+            completedProjects.projects = rows;
+        }
+
+        res.json(completedProjects);
+    })
+)
+router.get(
+    '/remaining',
+    [verbose],
+    asyncHandler(async (req, res) => {
+        handleValidation(req, 'GET Remaining Projects - ');
+        const { v } = req.query;
+        let remainingProjects = {
+            remaining: 0,
+            projects: {}
+        }
+        const params = [C_PROJECT.STATUS.TODO, C_PROJECT.STATUS.ON_HOLD]
+        const isRemainingSQL = `
+            SELECT *
+            FROM projects
+            WHERE completed_at IS NULL
+              AND status::TEXT = $1 OR status::TEXT = $2;
+        `
+        const { rows } = await query(isRemainingSQL, params);
+
+        remainingProjects.active = rows.length;
+        if ( v === 'true' ) {
+            remainingProjects.projects = rows;
+        }
+
+        res.json(remainingProjects);
+    })
+)
+router.get(
+    '/total',
+    [verbose],
+    asyncHandler(async (req, res) => {
+        handleValidation(req, 'GET Total Projects - ');
+        const { v } = req.query;
+        let totalProjects = {
+            total: 0,
+            projects: {}
+        }
+        const isTotalSQL = `
+            SELECT *
+            FROM projects
+        `
+        const { rows } = await query(isTotalSQL);
+
+        totalProjects.active = rows.length;
+        if ( v === 'true' ) {
+            totalProjects.projects = rows;
+        }
+
+        res.json(totalProjects);
+    })
+)
 
 module.exports = router;
