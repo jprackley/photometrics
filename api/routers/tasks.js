ee77ed7c12bc200411d1c359936ddf75a0346a07
JPack <168360639+jprackley@users.noreply.github.com>
2026-05-24 23:03:44 -0600

    Testing the Timer Start Stop API

--- api/routers/tasks.js
+++ api/routers/tasks.js
@@ -272,7 +272,7 @@ router.patch('/:id/timer/stop',
             UPDATE tasks
             SET 
                 stop_time = now(),
-                total_time = EXTRACT(EPOCH FROM (now() - start_time)) / 60,
+                total_time = EXTRACT(EPOCH FROM (now() - start_time)) / 60, --Time in Minutes
                 updated_at = now(),
                 completed_at = now()
             WHERE task_id = $1
