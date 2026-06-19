97fbe1b47af046c5ea639deb7dbb82f1df547c73
jprackley <168360639+jprackley@users.noreply.github.com>
2026-06-19 15:06:46 -0600

    Increase token max age to 1 day
    Extend the access token TTL in utils/constants/cAuth.js by changing TOKEN_MAX_AGE_MS from 1000 * 120 (2 minutes) to 1000 * 60 * 60 * 24 (1 day). The refresh token TTL remains at 1 week; inline comment was updated to reflect the new duration.

--- utils/constants/cAuth.js
+++ utils/constants/cAuth.js
@@ -1,6 +1,6 @@
 
 const SALT_ROUNDS = 10;
-const TOKEN_MAX_AGE_MS = 1000 * 120; // 1000ms * 120 = 2m
+const TOKEN_MAX_AGE_MS = 1000 * 60 * 60 * 24; // 1000ms * 60 * ... = 1d
 const REFRESH_TOKEN_MAX_AGE_MS = 1000 * 60 * 60 * 24 * 7; // 1 week
 
 module.exports = {
